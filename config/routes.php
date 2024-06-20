9440d65d216bbb124860a9a6ad6209f6321f25d0
khamidovichulugbek <khamidovichulugbek@gmail.com>
2024-06-20 07:32:31 +0500

    init

--- config/routes.php
+++ config/routes.php
@@ -13,6 +13,6 @@ return [
     Route::get('/login', [AuthController::class, 'index'],[AuthMiddlewares::class]),
     Route::post('/login', [AuthController::class, 'login']),
     Route::post('/logout', [AuthController::class, 'logout']),
-    Route::get('/profile', [AuthController::class, 'profile']),
+    Route::get('/profile', [AuthController::class, 'profile'], [GuestMiddlewares::class]),
     Route::post('/profile', [AuthController::class, 'update_profile']),
 ];
